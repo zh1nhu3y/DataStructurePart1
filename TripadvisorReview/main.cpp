70a0a5d29d9ab72db9ff7351b7aeb7f15eda115c
zhinhuey <panzhinhuey@gmail.com>
2024-10-05 20:05:38 +0800

    update

--- TripadvisorReview/main.cpp
+++ TripadvisorReview/main.cpp
@@ -30,24 +30,24 @@ int main()
 
     cout << "\n================= SENTIMENT ANALYSIS ================\n\n";
 
-    // reviews.loadFromFile("C:/Users/lawme/OneDrive - Asia Pacific University/Degree/Sem 2/Data Structure/Assignment/DSTR_P1_Data/organized_hotel_reviews_test.csv");
-    // // Create a SentimentLinkedList to analyze the reviews
-    // SentimentLinkedList sentimentList(positiveWords, negativeWords);
+    reviews.loadFromFile("C:/Users/lawme/OneDrive - Asia Pacific University/Degree/Sem 2/Data Structure/Assignment/DSTR_P1_Data/organized_hotel_reviews_test.csv");
+    // Create a SentimentLinkedList to analyze the reviews
+    SentimentLinkedList sentimentList(positiveWords, negativeWords);
 
-    // // Add each reviews to the linked list
-    // for (int i = 0; i < reviews.getReviewCount(); i++)
-    // {
-    //     Review review = reviews.getReview(i);
-    //     sentimentList.addReview(review.text, review.rating);
-    //     cout << "Added review " << i + 1 << " to the list." << endl;
-    // }
-    // cout << "Number of reviews loaded: " << sentimentList.getSize() << endl; // print number of reviews loaded
+    // Add each reviews to the linked list
+    for (int i = 0; i < reviews.getReviewCount(); i++)
+    {
+        Review review = reviews.getReview(i);
+        sentimentList.addReview(review.text, review.rating);
+        cout << "Added review " << i + 1 << " to the list." << endl;
+    }
+    cout << "Number of reviews loaded: " << sentimentList.getSize() << endl; // print number of reviews loaded
 
-    // // Sentiment Analysis for all reviews
-    // sentimentList.analyzeSentiment();
+    // Sentiment Analysis for all reviews
+    sentimentList.analyzeSentiment();
 
-    // // Save sentiment analysis results to a file
-    // sentimentList.saveResultsToFile("sentiment_analysis_results.txt");
+    // Save sentiment analysis results to a file
+    sentimentList.saveResultsToFile("sentiment_analysis_results.txt");
 
     cout << "\n================= SUMMARY REPORT ================\n\n";
 
